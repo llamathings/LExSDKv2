42d59aafe7da643b0a23727aa787de370b37fb42
Mgamerz <mgamerz@msn.com>
2026-01-23 20:56:13 -0700

    Add shared ELoadFlags

--- Src/LESDK/Common/Misc.hpp
+++ Src/LESDK/Common/Misc.hpp
@@ -65,6 +65,25 @@ struct FObjectExport : public FObjectResource
 #pragma pack(pop)
 
 
+// Flags used to control loading
+enum ELoadFlags
+{
+    LOAD_None = 0x00000000,	// No flags
+    LOAD_SeekFree = 0x00000001,	// Load the file using seekfree
+    LOAD_NoWarn = 0x00000002,	// Don't warn if there's a problem
+    LOAD_Throw = 0x00000008,	// Throw an exception if there's a problem
+    LOAD_Verify = 0x00000010,	// Test if it exists; don't actually load the file
+    LOAD_AllowDll = 0x00000020, // ???
+    LOAD_DisallowFiles = 0x00000040,	// ???
+    LOAD_NoVerify = 0x00000080,   // Don't verify imports exist
+    LOAD_Quiet = 0x00002000,   // Don't log anything
+    LOAD_FindIfFail = 0x00004000,	// If object fails to load, try to find in memory instead...?
+    LOAD_MemoryReader = 0x00008000,	// Load into memory, and then load from memory instead of piece-by-piece from disk
+    LOAD_RemappedPackage = 0x00010000,	// Unused in ME
+    LOAD_NoRedirects = 0x00020000,	// Ignore object redirectors
+};
+
+
 namespace LESDK {
 
     void InitializeConsole();
