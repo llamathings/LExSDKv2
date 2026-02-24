0a918c29fab3a2e7db799d8e9feb7d87626eedc1
Mgamerz <mgamerz@msn.com>
2026-02-24 08:47:06 -0700

    Common.hpp: Include new hpps

--- Src/LESDK/Common/Common.hpp
+++ Src/LESDK/Common/Common.hpp
@@ -27,6 +27,9 @@ namespace LESDK
 #include "LESDK/Common/SFXName.hpp"
 #include "LESDK/Common/TArray.hpp"
 #include "LESDK/Common/TMap.hpp"
+#include "LESDK/Common/TLinkedList.hpp"
+#include "LESDK/Common/RefCountPointer.hpp"
+#include "LESDK/Common/FArchive.hpp"
 
 // This header *must* be at the end.
 #include "LESDK/Common/Misc.hpp"
